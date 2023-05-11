689b173c787ff89737941623c590bdffed57ed21
jeremyaaronha <jeremyaaronha@gmail.com>
2023-05-11 01:30:30 -0400

    pagina comercio avanzada

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,25 @@
+const date = new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    year: 'numeric',
+    day: 'numeric',
+    month: 'long'
+  });
+
+document.getElementById("date").innerHTML = date;
+
+
+const year = new Date().getFullYear();
+document.getElementById('currentYear').textContent = year;
+
+const lastUpdated = new Date(document.lastModified);
+const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+};
+const formattedDate = lastUpdated.toLocaleDateString('en-US', options);
+document.getElementById('lastUpdated').textContent = formattedDate;
