73e9eb9bac1cb47560399787ed822a5a41f59a3b
jeremyaaronha <jeremyaaronha@gmail.com>
2023-07-17 19:21:47 -0400

    change sizes

--- final-proyect-wdd230/scripts/js.js
+++ final-proyect-wdd230/scripts/js.js
@@ -5,24 +5,51 @@ lastModifiedElement.textContent = lastModified.toLocaleString();
 
 
 // Make a GET request to the OpenWeatherMap API
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&appid=YOUR_API_KEY&units=metric')
-.then(response => response.json())
-.then(data => {
-  // Extract the required weather information from the API response
-  const temperature = data.main.temp;
-  const description = data.weather[0].description;
-  const humidity = data.main.humidity;
 
-  // Create the HTML content for the weather card
-  const weatherCard = document.getElementById('weather-card');
-  weatherCard.innerHTML = `
-  <h2>Weather</h2>
-  <p>Temperature: ${temperature}°C</p>
-  <p>Description: ${description}</p>
-  <p>Humidity: ${humidity}%</p>
-  `;
-})
-.catch(error => console.log(error));
+const apiKey = 'de64adba7154b478d2244bfca2081396';
+
+// Función para actualizar el contenido de los elementos HTML con los datos del clima
+function actualizarClima(temperatura, descripcion, humedad) {
+  const weatherCard = document.querySelector('.weather-individual-card');
+  weatherCard.querySelector('p:nth-child(2)').textContent = `Temperature: ${temperatura}°C`;
+  weatherCard.querySelector('p:nth-child(3)').textContent = `Condition Description: ${descripcion}`;
+  weatherCard.querySelector('p:nth-child(4)').textContent = `Humidity: ${humedad}%`;
+}
+
+// Función para actualizar el contenido de los elementos HTML con la previsión de temperatura
+function actualizarPrevisionTemperatura(forecast) {
+  const forecastCards = document.querySelectorAll('.forecast-temperature');
+
+  forecastCards.forEach((card, index) => {
+    const date = new Date(forecast[index + 1].dt * 1000).toLocaleDateString();
+    const temperatura = Math.round(forecast[index + 1].temp.day - 273.15); // Convertir temperatura de Kelvin a Celsius
+
+    card.textContent = `${date}: ${temperatura}°C`;
+  });
+}
+
+// Obtener datos del clima actual y previsión de temperatura
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    const temperatura = Math.round(data.main.temp - 273.15); // Convertir temperatura de Kelvin a Celsius
+    const descripcion = data.weather[0].description;
+    const humedad = data.main.humidity;
+
+    actualizarClima(temperatura, descripcion, humedad);
+
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=Carlsbad&cnt=4&appid=${apiKey}`);
+  })
+  .then(response => response.json())
+  .then(data => {
+    const forecast = data.list;
+
+    actualizarPrevisionTemperatura(forecast);
+  })
+  .catch(error => {
+    console.log('Error al obtener los datos del clima:', error);
+  });
+
 
 
 // Get the current total number of specialty drinks from localStorage
